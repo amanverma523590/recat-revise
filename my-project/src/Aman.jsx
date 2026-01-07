604808d21de4e2d24afa0bddadd4b40566306861
Aman Verma <aman97104@gmail.com>
2026-01-07 10:59:49 +0545

    setup

---
+++ my-project/src/Aman.jsx
@@ -0,0 +1,9 @@
+
+
+export function Aman(){
+    return(
+        <div>
+            <h1 className="text-primary">I am aman verma</h1>
+        </div>
+    )
+}
---
+++ my-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1>
+        <i className="bi bi-alarm me-2"></i>
+        Bootstrap Icon Test
+      </h1>
+
+      <button className="btn btn-success mt-3">
+        <i className="bi bi-check-circle me-2"></i>
+        Working
+      </button>
+    </div>
+  )
+}
+
+export default App
