d0ce058832033f7a3f2d2a974b708631db6e88b8
Aman Verma <aman97104@gmail.com>
2026-01-11 11:28:44 +0545

    two way

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { RemoteAPI } from './RemoteAPI';
+import { TwoWay } from './Two way Data Binding/Twoway';
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RemoteAPI/>
-  </StrictMode>,
+   <TwoWay/>
+  </StrictMode>
 )
