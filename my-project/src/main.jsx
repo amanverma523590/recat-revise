8c3bc4ca7e61209e1fa3472ef4a507bbbd0a4bb8
Aman Verma <aman97104@gmail.com>
2026-01-11 10:30:31 +0545

    shoping

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -2,12 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { FetchApi } from './FetchApi';
-
+import { RemoteAPI } from './RemoteAPI';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <FetchApi/>
+   <RemoteAPI/>
   </StrictMode>,
 )
