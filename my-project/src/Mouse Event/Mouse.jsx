05013778c58c8f195602ff2e5787d3ed8420d929
Aman Verma <aman97104@gmail.com>
2026-01-12 18:17:44 +0545

    mouseanimation

--- my-project/src/Mouse Event/Mouse.jsx
+++ my-project/src/Mouse Event/Mouse.jsx
@@ -1,39 +1,86 @@
-import { useEffect, useState } from "react";
-import axios from 'axios'
-import './mousedemo.css'
+// import { useEffect, useState } from "react";
+// import axios from 'axios'
+// import './mousedemo.css'
+
+// export function Mouse() {
+//   const [mobiles, setMobiles] = useState([{ img_src: null }]);
+//   const[previewSrc,setPreviewSrc] = useState('/assets/google.png')
+
+//     useEffect(()=>{
+//         axios.get(`mobile.json`)
+//         .then(response=>{
+//             console.log(response)
+//             setMobiles(response.data)
+//         })
+//     },[])
+
+//   function handleMouseOver(e){
+//     setPreviewSrc(e.target.src)
+//   }
+
+//   return (
+//     <div className="container-fluid">
+//       <div className="row mt-4">
+//         <div className="col-1">
+//             {
+//                 mobiles.map((mobile,i)=>(
+//                     <div key={i} className="my-4">
+//                         { (mobile.img_src) ? ( <img src={mobile.img_src} alt="noimg" height="50" width="50" onMouseOver={handleMouseOver} /> ) : ( <div>no img</div> ) }
+//                     </div>
+//                 ))
+//             }
+//         </div>
+//         <div className="col-11">
+//             <img src={previewSrc} alt="no img" width='300' heght='400' />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useState } from "react";
+import "./mouseanimation.css";
 
 export function Mouse() {
-  const [mobiles, setMobiles] = useState([{ img_src: null }]);
-  const[previewSrc,setPreviewSrc] = useState('/assets/google.png')
+  const [animation, setAnimation] = useState({
+    animationName: "spin",
+    animationDuration: "8s",
+    animationIterationCount: "infinite",
+    animationTimingFunction: "linear",
+  });
 
-    useEffect(()=>{
-        axios.get(`mobile.json`)
-        .then(response=>{
-            console.log(response)
-            setMobiles(response.data)
-        })
-    },[])
-
-
-  function handleMouseOver(e){
-    setPreviewSrc(e.target.src)
+  function handleMouseDown() {
+    setAnimation({
+      animationName: "spin",
+      animationDuration: "1s",
+      animationIterationCount: "infinite",
+      animationTimingFunction: "linear",
+    });
+  }
+  function handleMouseUp() {
+    setAnimation({
+      animationName: "spin",
+      animationDuration: "8s",
+      animationIterationCount: "infinite",
+      animationTimingFunction: "linear",
+    });
   }
 
   return (
-    <div className="container-fluid">
-      <div className="row mt-4">
-        <div className="col-1">
-            {
-                mobiles.map((mobile,i)=>(
-                    <div key={i} className="my-4">
-                        { (mobile.img_src) ? ( <img src={mobile.img_src} alt="noimg" height="50" width="50" onMouseOver={handleMouseOver} /> ) : ( <div>no img</div> ) }
-                    </div>
-                ))
-            }
-        </div>
-        <div className="col-11">
-            <img src={previewSrc} alt="no img" width='300' heght='400' />
-        </div>
+    <div
+      className="container-fluid d-flex justify-content-center align-items-center "
+      style={{ height: "100vh", backgroundColor: "gray" }}
+    >
+      <div>
+        <img
+          src="/assets/iphone.png"
+          alt="no img"
+          height={100}
+          width={100}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          style={animation}
+        />
       </div>
     </div>
   );
