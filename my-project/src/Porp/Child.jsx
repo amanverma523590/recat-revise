c3d5d516c441873837a6382eceaa20f0d1196d6a
Aman Verma <aman97104@gmail.com>
2026-01-16 18:04:39 +0545

    props

---
+++ my-project/src/Porp/Child.jsx
@@ -0,0 +1,23 @@
+
+
+// export function Child({user}){
+//     console.log(user)
+//     return(
+//         <div>
+//             <p>hi {user.name} </p>
+//             <p>i am {user.age} years old</p>
+//             <hr />
+//         </div>
+//     )
+// }
+
+export function Child({name}){
+
+    console.log(name)
+    return(
+        <div>
+            <h3>student name = {name}</h3>
+
+        </div>
+    )
+}
---
+++ my-project/src/Porp/Parent.jsx
@@ -0,0 +1,64 @@
+// import { Child } from "./child"
+
+import { useState } from "react";
+import { Child } from "./child";
+
+
+
+// export function Parent(){
+
+//     let info = {
+//         name:"aman",
+//         age:26,
+//         address:"nepalgunj",
+//         biodata:{
+//             ismarried:"nahi",
+//             islogin:"ha"
+//         }
+//     }
+//     let info2 = {
+//         name:"sanam",
+//         age:18,
+//         address:"nepalgunj",
+//         biodata:{
+//             ismarried:"nahi",
+//             islogin:"ha"
+//         }
+//     }
+//     let info3 = {
+//         name:"sahil",
+//         age:56,
+//         address:"nepalgunj",
+//         biodata:{
+//             ismarried:"nahi",
+//             islogin:"ha"
+//         }
+//     }
+
+//     return(
+//         <div>
+//             <h1>Props in react</h1>
+//             <Child user={info} />
+//             <Child user={info2} />
+//             <Child user={info3} />
+//         </div>
+//     )
+// }
+
+
+export function Parent(){
+
+    // let arr = ["aman","sanam","sahil","manish"]
+
+    const[change,setChange] = useState('aman')
+
+    return(
+        <div>
+           <h1>i am parent</h1>
+             { change  && <Child name={change}/> }
+             <button onClick={()=>setChange("sanam")}>update</button>
+        </div>
+    )
+}
+
+
