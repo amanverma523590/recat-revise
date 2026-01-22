8975f2faea431bd85d7e7d17bce5992f3b2d35dd
Aman Verma <aman97104@gmail.com>
2026-01-22 14:53:36 +0545

    useREdcuer  improvement

--- my-project/src/WeatherApi/useReducer.jsx
+++ my-project/src/WeatherApi/useReducer.jsx
@@ -1,28 +1,35 @@
 import { useReducer } from "react"
 
 
-
 export function ReducerHook(){
 
+    const initialState={
+        count : 0,
+    };
+
     function reducer(state,action){
-        console.log(state)
-        console.log(action)
+
 
         if(action.type==='Increment'){
-           return state +1;
+           return {count: state.count+1};
+        }else if(action.type==='Decrement'){
+            return {count: state.count-1};
         }else{
-            return state-1;
+            return ({count:0});
         }
     }
 
-
-    const[count,dispatch] = useReducer(reducer,0);
-    console.log(useReducer(reducer,0))
+    const[state,dispatch] = useReducer(reducer,initialState);
 
     return(
         <div>
             <button className="mx-4 mt-4 btn btn-warning" onClick={()=>dispatch({ type:"Increment" })}>Increment</button>
             <button className="mx-4 mt-4 btn btn-danger" onClick={()=>dispatch( {type:'Decrement'} )}>Decrement</button>
+            <button className="mx-4 mt-4 btn btn-info" onClick={()=>dispatch({type:'Reset'})}>Reset</button>
+            <h1>{state.count}</h1>
         </div>
     )
 }
+
+
+
