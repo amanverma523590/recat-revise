c3bcb0697225ad459bc3ba8c487cc4fab2700b41
Aman Verma <aman97104@gmail.com>
2026-02-10 17:31:57 +0530

    started practising

---
+++ my-project/src/Prac/Prac.jsx
@@ -0,0 +1,9 @@
+
+
+export function Prac(){
+    return(
+        <div>
+            <h1>I am Prac</h1>
+        </div>
+    )
+}
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { Parent } from '../practise/usecontext/Parent';
-
+import { Prac } from './Prac/Prac';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Parent/>
-  </BrowserRouter>
+  <StrictMode>
+     <Prac/>
+  </StrictMode>
+   
+  
 )
